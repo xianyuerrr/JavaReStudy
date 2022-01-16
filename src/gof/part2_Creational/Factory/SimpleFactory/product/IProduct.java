536dd7a1c4aac17ea3a6496b8d4344353e34cedf
xianyue <43592186+xianyueer@users.noreply.github.com>
2022-01-16 21:24:15 +0800

    重构结构，将 Gof 独立出来

--- src/gof/part2_Creational/Factory/SimpleFactory/product/IProduct.java
+++
@@ -1,9 +0,0 @@
-package gof.part2_Creational.Factory.SimpleFactory.product;
-
-/**
- * @auther xianyue
- * @date 2021/10/5 - 星期二 - 12:48
- **/
-public interface IProduct {
-    void doSomething();
-}
